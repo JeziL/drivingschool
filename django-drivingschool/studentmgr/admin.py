3d5a27d1c2c1a164ad6c79894e02d35185b8fff0
Li <me@wangjinli.com>
2018-01-22 22:57:43 +0800

    Add foreign keys to student model. - class type - enroller -- channel

--- django-drivingschool/studentmgr/admin.py
+++ django-drivingschool/studentmgr/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from .models import Student
+from .models import Student, ClassType, Enroller, Channel
 
 
 class StudentAdmin(admin.ModelAdmin):
@@ -8,8 +7,34 @@ class StudentAdmin(admin.ModelAdmin):
                     'mobile',
                     'idNo',
                     'licType',
+                    'classType',
                     'currentStage',
+                    'enroller',
                     'enrollDate')
 
 
+class ClassTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'licType',
+                    'price',
+                    'period',
+                    'createTime')
+
+
+class EnrollerAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'mobile',
+                    'channel')
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'channelType',
+                    'addr',
+                    'createTime')
+
+
 admin.site.register(Student, StudentAdmin)
+admin.site.register(ClassType, ClassTypeAdmin)
+admin.site.register(Enroller, EnrollerAdmin)
+admin.site.register(Channel, ChannelAdmin)
--- django-drivingschool/studentmgr/models.py
+++ django-drivingschool/studentmgr/models.py
@@ -38,6 +38,50 @@ STAGE_OPTIONS = (
     (4, '科目四'),
     (5, '拿证'),
 )
+PERIOD_OPTIONS = (
+    (0, '全周'),
+    (1, '周一至周五'),
+    (2, '周末')
+)
+CHANNEL_TYPE_OPTIONS = (
+    (0, '线下-门店直招'),
+    (1, '线下-挂牌分校'),
+    (2, '线下-内部职工'),
+    (3, '线下-代理网点'),
+    (4, '线上-营销活动'),
+    (5, '线上-平台合作'),
+)
+
+
+class Channel(models.Model):
+    name = models.CharField('渠道名称', max_length=20)
+    channelType = models.IntegerField('渠道类型', choices=CHANNEL_TYPE_OPTIONS, default=0)
+    addr = models.CharField('地址', max_length=200)
+    note = models.TextField('备注', null=True, blank=True)
+    createTime = models.DateTimeField('创建时间', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Enroller(models.Model):
+    name = models.CharField('姓名', max_length=20)
+    mobile = models.CharField('手机号', max_length=15)
+    channel = models.ForeignKey(Channel, verbose_name='渠道', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class ClassType(models.Model):
+    name = models.CharField('班型名称', max_length=20)
+    licType = models.IntegerField('驾照类型', choices=LICENSE_TYPE_OPTIONS, default=5)
+    price = models.CharField('班型价格', max_length=5)
+    period = models.IntegerField('学车时段', choices=PERIOD_OPTIONS, default=0)
+    createTime = models.DateTimeField('创建时间', default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 
 class Student(models.Model):
@@ -55,5 +99,10 @@ class Student(models.Model):
     currentStage = models.IntegerField('当前科目', choices=STAGE_OPTIONS, default=0)
     enrollDate = models.DateTimeField('报名时间', default=timezone.now)
     note = models.TextField('备注', null=True, blank=True)
+    classType = models.ForeignKey(ClassType, verbose_name='报名班型', null=True, on_delete=models.SET_NULL)
+    enroller = models.ForeignKey(Enroller, verbose_name='招生代表', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
 
 
