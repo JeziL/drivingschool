3ffa9f9db834f7b8c2bd1875844bc70be7a3ae5b
Li <me@wangjinli.com>
2018-01-23 17:18:02 +0800

    Some admin l10n.

--- django-drivingschool/studentmgr/apps.py
+++ django-drivingschool/studentmgr/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class StudentmgrConfig(AppConfig):
     name = 'studentmgr'
+    verbose_name = '驾校管理'
--- django-drivingschool/studentmgr/models.py
+++ django-drivingschool/studentmgr/models.py
@@ -54,6 +54,9 @@ CHANNEL_TYPE_OPTIONS = (
 
 
 class Channel(models.Model):
+    class Meta:
+        verbose_name = '渠道'
+        verbose_name_plural = '渠道'
     name = models.CharField('渠道名称', max_length=20)
     channelType = models.IntegerField('渠道类型', choices=CHANNEL_TYPE_OPTIONS, default=0)
     addr = models.CharField('地址', max_length=200)
@@ -65,6 +68,9 @@ class Channel(models.Model):
 
 
 class Enroller(models.Model):
+    class Meta:
+        verbose_name = '招生代表'
+        verbose_name_plural = '招生代表'
     name = models.CharField('姓名', max_length=20)
     mobile = models.CharField('手机号', max_length=15)
     channel = models.ForeignKey(Channel, verbose_name='渠道', null=True, on_delete=models.SET_NULL)
@@ -74,6 +80,9 @@ class Enroller(models.Model):
 
 
 class ClassType(models.Model):
+    class Meta:
+        verbose_name = '班型'
+        verbose_name_plural = '班型'
     name = models.CharField('班型名称', max_length=20)
     licType = models.IntegerField('驾照类型', choices=LICENSE_TYPE_OPTIONS, default=5)
     price = models.CharField('班型价格', max_length=5)
@@ -85,6 +94,9 @@ class ClassType(models.Model):
 
 
 class Student(models.Model):
+    class Meta:
+        verbose_name = '学员'
+        verbose_name_plural = '学员'
     name = models.CharField('姓名', max_length=20)
     sex = models.IntegerField('性别', choices=SEX_OPTIONS, default=0)
     idNo = models.CharField('证件号', max_length=20)
