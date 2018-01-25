13817ab3fdb61cae5808272fddbe897948e45749
Li <me@wangjinli.com>
2018-01-25 17:14:21 +0800

    Use class type price as fee's default value.

--- django-drivingschool/studentmgr/static/admin/js/student_detail.js
+++ django-drivingschool/studentmgr/static/admin/js/student_detail.js
@@ -14,5 +14,13 @@ $(document).ready(function () {
             $('.field-origLicType, .field-licChangeDate').slideUp('medium');
         }
     });
+
+    $('div.add-row a').click(function () {
+        var price_str = $('div.field-class_type_price p').text();
+        var price = parseInt(price_str, 10);
+        $('div.field-money input').each(function () {
+            $(this).val(price);
+        })
+    })
 });
 
