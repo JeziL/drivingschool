9683f242ae08633508b3872fa52fb1225497c58c
Li <me@wangjinli.com>
2018-01-26 10:44:46 +0800

    Add print button.

--- django-drivingschool/studentmgr/static/admin/js/student_detail.js
+++ django-drivingschool/studentmgr/static/admin/js/student_detail.js
@@ -15,13 +15,30 @@ $(document).ready(function () {
         }
     });
 
+    addPrintButton();
     $('div.add-row a').click(function () {
         var price_str = $('div.field-class_type_price p').text();
         $('div.field-money input').each(function () {
             if ($(this).val() === '') {
                 $(this).val(price_str);
             }
-        })
-    })
+        });
+        addPrintButton();
+    });
 });
 
+function addPrintButton() {
+    $('div.dynamic-fee_set h3').each(function () {
+        if ($(this).children().length === 3) {
+            $(this).append("<input type='button' class='printBtn' style='float: right;' value='打印培训费' />");
+        }
+    });
+    $('input.printBtn').each(function () {
+        $(this).unbind('click');
+        $(this).click(onPrintBtnClicked);
+    })
+}
+
+function onPrintBtnClicked() {
+    console.log("button click.");
+}
