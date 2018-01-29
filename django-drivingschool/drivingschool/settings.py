507cd1ddb76d55fb07bc0d7967c1fb1f5d2433e9
Li <me@wangjinli.com>
2018-01-29 20:23:08 +0800

    Turn off debug.

--- django-drivingschool/drivingschool/settings.py
+++ django-drivingschool/drivingschool/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i%*5c)69n#v9slr&vdfts!sp57^dg91_mxmzeb=o#l5nxegnkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
