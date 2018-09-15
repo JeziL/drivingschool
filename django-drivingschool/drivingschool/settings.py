7aa6b8ab09912c0dddd680c92e74ee19a9830218
Li <me@wangjinli.com>
2018-09-15 22:05:32 +0800

    Determine debug mode dynamically.

--- django-drivingschool/drivingschool/settings.py
+++ django-drivingschool/drivingschool/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import platform
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i%*5c)69n#v9slr&vdfts!sp57^dg91_mxmzeb=o#l5nxegnkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = platform.system() == 'Darwin'
 
 ALLOWED_HOSTS = ['*']
 
